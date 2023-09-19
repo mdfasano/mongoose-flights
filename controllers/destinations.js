e66ac7287bda568bf1d34603750699726d322ef8
mdfasano <mdfasano16@gmail.com>
2023-09-19 16:40:18 -0400

    part 2

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,14 +1,21 @@
 const Flight = require('../models/flight');
 
 async function create (req, res) {
-
-    res.redirect('flights/index');
+    const thisFlight = await Flight.findById(req.params.id);
+    thisFlight.destinations.push(req.body);
+    console.log(thisFlight)
+    try {
+        await thisFlight.save();
+    } catch (err) {
+        console.log(err);
+    }
+    res.redirect('/flights');
 }
 
 function newDestination (req, res) {
     res.render('flights/destinations/new', {
-
-    })
+        flightId: req.params.id
+    });
 }
 module.exports = {
     new: newDestination,
--- routes/destinations.js
+++ routes/destinations.js
@@ -2,6 +2,7 @@ const express = require('express');
 const destinationsController = require('../controllers/destinations');
 const router = express.Router();
 
-router.get('/:id/destinations/new', destinationsController.new);
+router.get('/flights/:id/destinations/new', destinationsController.new);
+router.post('/:id/destinations/create', destinationsController.create);
 
 module.exports = router;
